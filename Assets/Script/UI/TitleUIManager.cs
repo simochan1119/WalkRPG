62c90afb3c2aa643ecd1315054dd4b976689a2ee
simochan1119 <100325348+simochan1119@users.noreply.github.com>
2026-05-12 16:39:17 +0900

    202605121

--- Assets/Script/UI/TitleUIManager.cs
+++ Assets/Script/UI/TitleUIManager.cs
@@ -7,6 +7,9 @@ public class TitleUIManager : MonoBehaviour
     [Header("UI")]
     public TMP_InputField nameInput;
     public TMP_Text infoText;
+    [Header("Popup")]
+    public GameObject activityPopup;
+    public GameObject notificationPopup;
 
     private const string VillageSceneName = "02_Village";
 
@@ -23,6 +26,17 @@ public class TitleUIManager : MonoBehaviour
         {
             infoText.text = "ユーザー名を入力してください";
         }
+
+        if (!PermissionManager.Instance.HasActivityPermission)
+        {
+            activityPopup.SetActive(true);
+        }
+
+        // 通知権限
+        if (!PermissionManager.Instance.HasNotificationPermission)
+        {
+            notificationPopup.SetActive(true);
+        }
     }
 
     public async void OnClickGameStart()
@@ -32,5 +46,25 @@ public class TitleUIManager : MonoBehaviour
         await FirebaseManager.Instance.CreateOrUpdatePlayer(nameInput.text);
 
         SceneManager.LoadScene(VillageSceneName);
+    }
+    public void OnClickNotificationAllow()
+    {
+        PermissionManager.Instance.RequestNotificationPermission();
+
+        notificationPopup.SetActive(false);
+    }
+    public void OnClickNotificationLater()
+    {
+        notificationPopup.SetActive(false);
+    }
+    public void OnClickActivityAllow()
+    {
+        PermissionManager.Instance.RequestActivityPermission();
+
+        activityPopup.SetActive(false);
+    }
+    public void OnClickActivityLater()
+    {
+        activityPopup.SetActive(false);
     }
 }
